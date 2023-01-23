b57d81b14afc0781f0278ee37dca0c959f27a669
Paul Figueroa <pjsfigueroa@gmail.com>
2023-01-23 20:35:24 +0800

    Clean up another netlify flagged warnings.

--- src/components/cart/cart-dropdown.component.jsx
+++ src/components/cart/cart-dropdown.component.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import Button from '../button/button.component'
 import CartItem from './cart-item/cart-item.component'
 
-import { selectCartItems, selectIsCartOpen, selectCartTotal } from '../../store/cart/cart.selector'
+import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector'
 import { setIsCartOpen, removeAllItemsFromCart } from '../../store/cart/cart.action'
 
 const CartDropDown = () => {
@@ -23,7 +23,7 @@ const CartDropDown = () => {
                 dispatch(setIsCartOpen(false))
             }
         })
-    }, [])
+    })
 
     // toggle
     const toggleIsCartOpen = () => dispatch(setIsCartOpen(false))
