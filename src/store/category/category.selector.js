392c6fbdecf88df4468baafcd8ae4f18960dfc74
Paul Figueroa <pjsfigueroa@gmail.com>
2023-01-26 21:48:10 +0800

    Fix reduce() in category.selector.

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -11,7 +11,7 @@ export const selectCategories = createSelector(
 // Memoized, this will run whenever categories change
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) => categories.reduce((acc, categ) => {
+    (categoriesSlice) => categoriesSlice.reduce((acc, categ) => {
         const { category, items } = categ;
         acc[category.toLowerCase()] = items;
         return acc;
